527b82027dbc8fb254c821a2f3084c38a9355db1
JWLK-HASLO <jewels@haslo.co>
2020-04-04 20:36:26 +0900

    RN-UI: flexbox - 1

--- App.js
+++ App.js
@@ -2,18 +2,36 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.left}></View>
+            <View style={styles.right}>
+                <View style={styles.rightTop}></View>
+                <View style={styles.rightBottom}></View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    left: {
+        flex: 1,
+        backgroundColor: 'red',
+    },
+    right: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    rightTop: {
+        flex: 1,
+        backgroundColor: 'blue',
+    },
+    rightBottom: {
+        flex: 1,
+        backgroundColor: 'yellow',
+    },
 });
